cb8202319a6df5dfe3be5c4aa7c697360dff7d67
Simon11287 <71406841+Simon11287@users.noreply.github.com>
2020-11-15 19:24:42 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -21,5 +21,16 @@ example:
 Enter a name: Lebron
 That name is on the list
 """
-
+import  math
 nameList = ("Lebron","Kobe","Michale","Shaq","Dennis")
+user=(input("Enter a name")).strip()  
+count=1
+for i in names:
+   if i== user:
+      count=count-1
+      print("That name is on the list")   
+   if count>=5:
+      print("That name is not on the list") 
+   if i!=user:
+      count=count+1
+      continue 
